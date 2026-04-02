50fe86d32bbecb0295b9ce1b5e9858313c6f6fe2
pewnaosoba5 <pewnaosoba.abcdef@gmail.com>
2026-04-02 20:15:37 +0200

    Update Login.tsx

--- src/app/pages/auth/login/Login.tsx
+++ src/app/pages/auth/login/Login.tsx
@@ -1,14 +1,72 @@
-import React, { useMemo } from 'react'; import { Box, Text, color } from 'folds'; import { useSearchParams } from 'react-router-dom'; import { SSOAction } from 'matrix-js-sdk'; import { useAuthFlows } from '../../../hooks/useAuthFlows'; import { useAuthServer } from '../../../hooks/useAuthServer'; import { useParsedLoginFlows } from '../../../hooks/useParsedLoginFlows'; import { SSOLogin } from '../SSOLogin'; import { TokenLogin } from './TokenLogin'; import { getLoginPath } from '../../pathUtils'; import { usePathWithOrigin } from '../../../hooks/usePathWithOrigin'; import { LoginPathSearchParams } from '../../paths'; import { useClientConfig } from '../../../hooks/useClientConfig';
+import React, { useMemo } from 'react';
+import { Box, Text, color } from 'folds';
+import { useSearchParams } from 'react-router-dom';
+import { SSOAction } from 'matrix-js-sdk';
+import { useAuthFlows } from '../../../hooks/useAuthFlows';
+import { useAuthServer } from '../../../hooks/useAuthServer';
+import { useParsedLoginFlows } from '../../../hooks/useParsedLoginFlows';
+import { SSOLogin } from '../SSOLogin';
+import { TokenLogin } from './TokenLogin';
+import { getLoginPath } from '../../pathUtils';
+import { usePathWithOrigin } from '../../../hooks/usePathWithOrigin';
+import { LoginPathSearchParams } from '../../paths';
+import { useClientConfig } from '../../../hooks/useClientConfig';
 
-const getLoginTokenSearchParam = () => { const parmas = new URLSearchParams(window.location.search); const loginToken = parmas.get('loginToken'); return loginToken ?? undefined; };
+const getLoginTokenSearchParam = () => {
+  const parmas = new URLSearchParams(window.location.search);
+  const loginToken = parmas.get('loginToken');
+  return loginToken ?? undefined;
+};
 
-const useLoginSearchParams = (searchParams: URLSearchParams): LoginPathSearchParams => useMemo( () => ({ username: searchParams.get('username') ?? undefined, email: searchParams.get('email') ?? undefined, loginToken: searchParams.get('loginToken') ?? undefined, }), [searchParams] );
+const useLoginSearchParams = (searchParams: URLSearchParams): LoginPathSearchParams =>
+  useMemo(
+    () => ({
+      username: searchParams.get('username') ?? undefined,
+      email: searchParams.get('email') ?? undefined,
+      loginToken: searchParams.get('loginToken') ?? undefined,
+    }),
+    [searchParams]
+  );
 
-export function Login() { const server = useAuthServer(); const { hashRouter } = useClientConfig(); const { loginFlows } = useAuthFlows(); const [searchParams] = useSearchParams(); const loginSearchParams = useLoginSearchParams(searchParams); const ssoRedirectUrl = usePathWithOrigin(getLoginPath(server)); const loginTokenForHashRouter = getLoginTokenSearchParam(); const absoluteLoginPath = usePathWithOrigin(getLoginPath(server));
+export function Login() {
+  const server = useAuthServer();
+  const { hashRouter } = useClientConfig();
+  const { loginFlows } = useAuthFlows();
+  const [searchParams] = useSearchParams();
+  const loginSearchParams = useLoginSearchParams(searchParams);
+  const ssoRedirectUrl = usePathWithOrigin(getLoginPath(server));
+  const loginTokenForHashRouter = getLoginTokenSearchParam();
+  const absoluteLoginPath = usePathWithOrigin(getLoginPath(server));
 
-if (hashRouter?.enabled && loginTokenForHashRouter) { window.location.replace( ${absoluteLoginPath}?loginToken=${encodeURIComponent(loginTokenForHashRouter)} ); }
+  if (hashRouter?.enabled && loginTokenForHashRouter) {
+    window.location.replace(
+      `${absoluteLoginPath}?loginToken=${encodeURIComponent(loginTokenForHashRouter)}`
+    );
+  }
 
-const parsedFlows = useParsedLoginFlows(loginFlows.flows);
+  const parsedFlows = useParsedLoginFlows(loginFlows.flows);
 
-return ( Join the room on Cinny {parsedFlows.token && loginSearchParams.loginToken && ( )} {parsedFlows.sso && ( )} {!parsedFlows.sso && ( <Text style={{ color: color.Critical.Main }}> Login is not available on this server. )} ); }
-
+  return (
+    <Box direction="Column" gap="500">
+      <Text size="H2" priority="400">
+        Join the room on Cinny
+      </Text>
+      {parsedFlows.token && loginSearchParams.loginToken && (
+        <TokenLogin token={loginSearchParams.loginToken} />
+      )}
+      {parsedFlows.sso && (
+        <SSOLogin
+          providers={parsedFlows.sso.identity_providers}
+          redirectUrl={ssoRedirectUrl}
+          action={SSOAction.LOGIN}
+          saveScreenSpace={false}
+        />
+      )}
+      {!parsedFlows.sso && (
+        <Text style={{ color: color.Critical.Main }}>
+          Login is not available on this server.
+        </Text>
+      )}
+    </Box>
+  );
+}
